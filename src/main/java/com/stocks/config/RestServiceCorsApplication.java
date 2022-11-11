dcd5b6cb3ef745f044ae86ab120c343a5ca3c021
anthonyg876 <anthonygravier2@gmail.com>
2022-11-11 14:09:16 -0500

    Added Cors configuration

---
+++ src/main/java/com/stocks/config/RestServiceCorsApplication.java
@@ -0,0 +1,21 @@
+package com.stocks.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RestServiceCorsApplication {
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500");
+            }
+        };
+    }
+
+}
